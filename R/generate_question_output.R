b7abe080ae4d5e00f4e7b12046964864df0699f1
Ulrich Matter <ulrich.matter@unibas.ch>
2018-09-29 18:28:06 +0200

    initial commit

---
+++ R/generate_question_output.R
@@ -0,0 +1,96 @@
+##' Generate Question Text
+##'
+##' Generates Rmd strings for a set of questions
+##' @usage generate_question_text(x)
+##' @param x list, result of parsing file with questions via read_questions/select_questions
+##' @param type_headers list, indicating the headers to be used for each question type section (see example)
+##' @return a list of character strings (with markdown syntax)
+##' @details The...
+##' @author Ulrich Matter <umatter@protonmail.com>
+##' @depends 
+##' @examples
+##' myfile <- "data/exam_questions.xlsx"
+##' my_questions <- read_questions(myfile)
+##' selection <-select_questions(my_questions)
+##' generate_question_text(selection)
+##' @export
+##' 
+
+
+generate_question_text <-
+     function(x, type_headers = list(tf="## Part 1: TRUE/FALSE Questions\nFor each of the following statements, decide whether the statement is TRUE (T) or FALSE (F).\n",
+                                     mc="## Part 2: Multiple Choice Type A\nFor each of the following questions/statements, decide which of the possible responses are correct. NOTE: Either ONE OR SEVERAL of the response statements can be correct. In order to get a point, all correct response statements need to be marked!\n",
+                                     one_correct="## Part 3: Multiple Choice  Type B (only one correct!)\nFor each of the following questions/statements, decide which of the possible responses is correct. NOTE: For each question only ONE of the possible responses is correct. Select ONLY ONE of the response statements per question!\n")) {
+          
+          # process TRUE/FALSE questions
+          # select input
+          tfs <- x$tf
+          tfs$question_nr <- paste0(1:nrow(tfs), ".  ")
+          tfs_header <- type_headers$tf
+          
+          # create text output
+          tf_text <- paste0(tfs$question_nr,
+                            tfs$question, collapse = "\n")
+          
+          tf_text <- paste0(tfs_header, "\n",
+                            tf_text, "\n")
+          
+          # process MC questions
+          mcs <- x$mc
+          mcs$question_nr <- paste0(1:nrow(mcs), ".  ")
+          mcs_header <- type_headers$mc
+          
+          # create text output
+          # questions
+          q_text<- paste0(mcs$question_nr,
+                            mcs$question, "\n")
+          # responses per question
+          responses_text <- list()
+          for (i in 1:nrow(mcs)) {
+               
+          responses_text[[i]] <- 
+               paste0(toupper(letters[1:length(mcs[i,]$choices_solutions[[1]]$choices)]),
+                      ") ",
+                      trimws(as.character(mcs[i,]$choices_solutions[[1]]$choices)),
+                      "\n",
+                      collapse = "")
+          }
+          responses_text <- unlist(responses_text)
+          # put it together
+          mcs_text <- paste0(mcs_header, "\n", q_text, responses_text )
+     
+          
+          # process MC questions (only one correct)
+          mcs_oc <- x$one_correct
+          mcs_oc$question_nr <- paste0(1:nrow(mcs_oc), ".  ")
+          mcs_oc_header <- type_headers$one_correct
+          
+          # create text output
+          # questions
+          q_text_oc<- paste0(mcs_oc$question_nr,
+                          mcs_oc$question, "\n")
+          # responses per question
+          responses_text_oc <- list()
+          for (i in 1:nrow(mcs_oc)) {
+               
+               responses_text_oc[[i]] <- 
+                    paste0(toupper(letters[1:length(mcs_oc[i,]$choices_solutions[[1]]$choices)]),
+                           ") ",
+                           trimws(as.character(mcs_oc[i,]$choices_solutions[[1]]$choices)),
+                           "\n",
+                           collapse = "")
+          }
+          responses_text_oc <- unlist(responses_text_oc)
+          # put it together
+          mcs_oc_text <- paste0(mcs_oc_header, "\n", q_text_oc, responses_text_oc )
+          
+          
+          # Put all questions in one text
+          all_text <- paste0(tf_text, "\n",
+                             mcs_text,"\n",
+                             mcs_oc_text)
+          
+          
+          return(all_text)
+          
+}
---
+++ R/read_questions.R
@@ -0,0 +1,85 @@
+##' Read Exam Questions
+##'
+##' Reads an excel sheet with exam question of different types (T/F, MC, etc.)
+##' @usage read_questions(file)
+##' @param file character, the path/filename of the excel-sheet containing the questions to be read
+##' @param year character vector indicating from which years questions should be selected, if empty (NULL) questions from all years are selected.
+##' @param mock logical, if TRUE, only mock exam questions are selected (FALSE by default)
+##' @param topic character vector, indicating which question topics should be selected ('topic' column), defaults to NULL
+##' @return a list
+##' @details The...
+##' @author Ulrich Matter <umatter@protonmail.com>
+##' @depends readxl, readr
+##' @examples
+##' myfile <- "data/exam_questions.xlsx"
+##' my_questions <- read_questions(my_file)
+##' head(my_questions)
+##' @export
+##' 
+
+read_questions <- 
+     function(file, year=NULL, mock=FALSE, topic = NULL) {
+          require(readxl)
+          require(readr)
+          
+          # read the file, if not excel-file, assume csv
+          if (grepl("\\.xlsx", file)) {
+               q <- read_xlsx(file)
+               
+          } else {
+               if (grepl("\\.xls", file)) {
+                    q <- read_xls(file)
+               
+               } else {
+                         q <- read_csv(file)
+                    }
+          }
+          
+          # select only mock or real exam questions
+          if (mock==TRUE){
+               q <- q[q$used_in=="mock" & !is.na(q$used_in), ]
+          } 
+          
+          if (mock==FALSE) {
+               q <- q[q$used_in!="mock", ]
+          }
+          
+          # select only from certain year
+          q <- q[q$year==year,]
+          
+          # select certain topics 
+          if (!is.null(topic)) {
+               q <- q[q$topic %in% topic]
+          }
+          
+          
+          # parse different question types
+          # TRUE/FALSE questions
+          tf_questions <- q[q$type=="tf", c("question", "solution", "year", "used_in")]
+          
+          # Multiple choice (several correct)
+          mc <- q[q$type=="mc", c("question", "choices", "solution", "year", "used_in")]
+          choices <- strsplit(mc$choices, ";")
+          solutions <- strsplit(mc$solution, ";")
+          choices_solutions  <- lapply(1:length(choices), FUN = function(i){
+               data.frame(choices=choices[[i]], solutions=solutions[[i]])
+          })
+          mc_questions <- mc[, c("question", "year", "used_in")]
+          mc_questions$choices_solutions <- choices_solutions
+          
+          # Multiple choice, one correct
+          one_correct <- q[q$type=="one_correct", c("question", "choices", "solution", "year", "used_in")]
+          choices_oc <- strsplit(one_correct$choices, ";")
+          solutions_oc <- one_correct$solution
+          choices_solutions_oc  <- lapply(1:length(choices_oc), FUN = function(i){
+               data.frame(choices=choices_oc[[i]], solutions=solutions_oc[i]==choices_oc[[i]])
+          })
+          one_correct_questions <- one_correct[, c("question", "year", "used_in")]
+          one_correct_questions$choices_solutions <- choices_solutions_oc
+          
+          
+          # combine all for further processing in other functions
+          parsed_questions <- list(tf=tf_questions, mc=mc_questions, one_correct=one_correct_questions)
+          
+          return(parsed_questions)
+     }
