e51095559aa50ac51776dfdc6b0a1c589e00269d
umatter <umatter@protonmail.com>
2018-12-18 12:21:55 +0100

    fix minor bug

--- R/generate_solutions_output.R
+++ R/generate_solutions_output.R
@@ -69,7 +69,7 @@ generate_solution_text <-
 
                # put all together
                responses_colored <- paste0(response_letters_colored,
-                                   trimws(as.character(mcs[i,]$choices_solutions[[1]]$choices)), "\n")
+                                   trimws(as.character(mcs[i,]$choices_solutions[[1]]$choices)), "\n\n")
                responses_text[[i]] <- paste0(responses_colored, collapse = "")
                # solutions for correction output
                responses_letters[[i]] <- paste(response_letters[solutions], collapse = "|") # FormScan deliminates multiple choice responses with the pipe character!
@@ -108,7 +108,7 @@ generate_solution_text <-
                response_letter_colored <- paste0("\n\\textcolor{red}{", response_letter, ")} ")
                response_letter_colored[solutions] <- paste0("\n\\textcolor{green}{", response_letter[solutions], ")} ")
                responses_colored <- paste0(response_letter_colored,
-                                   trimws(as.character(mcs_oc[i,]$choices_solutions[[1]]$choices)), "\n")
+                                   trimws(as.character(mcs_oc[i,]$choices_solutions[[1]]$choices)), "\n\n")
                # put it all together
                responses_text_oc[[i]] <- paste0(responses_colored, collapse = "")
                # for output correction
